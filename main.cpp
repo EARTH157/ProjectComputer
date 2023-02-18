9c0d2a7133a4ffa5d48173f1d2b9f3865cb38297
EARTH1157 <earthzz.jirapat@outlook.com>
2023-02-18 20:03:44 +0700

    finish

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include<iostream>
-#include<iomanip>
-#include<windns.h>
-#include<cmath>
 #include<ctime>
-#include<cstddef>
+#include<string>
+#include<cmath>
 using namespace std;
 
+void menu(string name,int age){
+    cout << "input name : ";
+    getline(cin,name);
+    cout << "input age : ";
+    cin >> age;
+}
+
+void random(int number){
+    srand(time(0));
+    int a = rand()%78+1;
+}
+
 int main(){
-    cout << "Hi";
+    string namel;
+    int agel,a;
+    random(a);
+    cout << a << endl;
+    menu(namel,agel);
+    cout << namel << "\n" << agel;
     return 0;
 }
+
