cd29a4ae3cb4f902960d6d5c952e9c80a72db009
EARTH1157 <earthzz.jirapat@outlook.com>
2023-02-18 14:28:36 +0700

    main

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+#include<windns.h>
+#include<cmath>
+#include<ctime>
+#include<cstddef>
+using namespace std;
+
+int main(){
+    cout << "Hi";
+    return 0;
+}
